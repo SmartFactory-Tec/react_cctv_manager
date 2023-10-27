0b1c4ca2df5b1268d6ed3cf479db740ce33bc805
teapot2 <A01197460@itesm.mx>
2023-10-27 10:19:09 -0600

    Implement dynamic canvas updates for real-time camera data rendering

--- src/App.js
+++ src/App.js
@@ -1,72 +1,44 @@
 import "./App.css";
 import "./assets/styles/global.css";
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { Row, Col, Container } from "react-bootstrap";
 import Sidebar from "./components/sidebar/Sidebar.js";
 import { parseCameraData } from "./utils/apiUtils";
 
 function App() {
-  // const [cameraData, setCameraData] = useState([]);
-
-  const canvasRef1 = useRef(null);
-  const canvasRef2 = useRef(null);
-  const canvasRef3 = useRef(null);
-  const canvasRef4 = useRef(null);
-
-  // useEffect(() => {
-  //   canvasRefs.current = cameraData.map(() => React.createRef());
-  // }, [cameraData]);
-
-  const updateCanvas = (data) => {
-    const cameraId = data.camera_id;
-    const frame = data.frame;
-
-    switch (cameraId) {
-      case 0:
-        return parseCameraData(frame, canvasRef1);
-      case 1:
-        return parseCameraData(frame, canvasRef2);
-      case 2:
-        return parseCameraData(frame, canvasRef3);
-      case 3:
-        return parseCameraData(frame, canvasRef4);
-
-      default:
-        return null;
-    }
-  };
+  const [cameraData, setCameraData] = useState([]);
+  const canvasRefs = useRef([]);
 
   useEffect(() => {
+    // Iterate through the cameraData array and render each camera frame onto the corresponding canvas
+    cameraData.forEach((camera, i) => {
+      const { camera_id, frame } = camera;
+
+      // Call the function to parse and render the camera frame data onto the respective canvas
+      return parseCameraData(frame, canvasRefs.current[camera_id]);
+    });
+  }, [cameraData]);
+
+  useEffect(() => {
+    // Establish a WebSocket connection with the provided URL
     const socket = new WebSocket("ws://127.0.0.1:8000/ws/router/");
 
+    // Handle incoming messages from the WebSocket server
     socket.onmessage = (event) => {
       const data = JSON.parse(event.data); // Parse the JSON data
-      console.log(data);
-      updateCanvas(data);
+      setCameraData((prevData) => {
+        // Create a new array based on the previous data and add the new data
+        const newData = [...prevData];
+        newData[data.camera_id] = data;
+        return newData;
+      });
     };
 
+    // Close the WebSocket connection when the component is unmounted
     return () => {
       socket.close();
     };
   }, []);
-
-  //useEffect(() => {
-  //  cameraData.forEach((camera, index) => {
-  //    try {
-  //      const canvas = canvasRefs.current[index]?.current;
-  //      if (!canvas) return;
-  //      const ctx = canvas.getContext("2d");
-  //      const img = new Image();
-  //
-  //      img.onload = () => {
-  //        ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
-  //      };
-  //      img.src = `data:image/jpeg;base64,${camera.frame}`;
-  //    } catch (error) {
-  //      console.error(error);
-  //    }
-  //  });
-  //}, [cameraData]);
 
   return (
     <div className="App">
@@ -80,38 +52,22 @@ function App() {
 
               <Col xs={10} className="px-4">
                 <Row>
-                  <Col xs={6}>
-                    <canvas
-                      key={"camera1"}
-                      ref={canvasRef1}
-                      width={720}
-                      height={480}
-                    />
-                  </Col>
-                  <Col xs={6}>
-                    <canvas
-                      key={"camera2"}
-                      ref={canvasRef2}
-                      width={720}
-                      height={480}
-                    />
-                  </Col>
-                  <Col xs={6}>
-                    <canvas
-                      key={"camera3"}
-                      ref={canvasRef3}
-                      width={720}
-                      height={480}
-                    />
-                  </Col>
-                  <Col xs={6}>
-                    <canvas
-                      key={"camera4"}
-                      ref={canvasRef4}
-                      width={720}
-                      height={480}
-                    />
-                  </Col>
+                  {cameraData.map((camera, i) => {
+                    console.log(canvasRefs.current[i]);
+
+                    return (
+                      <Col xs={6}>
+                        <canvas
+                          key={"camera_" + i}
+                          ref={(ref) => {
+                            canvasRefs.current[i] = ref;
+                          }}
+                          width={720}
+                          height={480}
+                        />
+                      </Col>
+                    );
+                  })}
                 </Row>
               </Col>
             </Row>
--- src/utils/apiUtils.js
+++ src/utils/apiUtils.js
@@ -1,15 +1,26 @@
+/**
+ * Parse and render camera frame data onto the canvas.
+ * @param {string} frame - The camera frame data in base64 format.
+ * @param {Object} canvasRef - Reference to the canvas element.
+ */
 export const parseCameraData = (frame, canvasRef) => {
-    try {
-      const canvas = canvasRef.current;
-      if (!canvas) return;
-      const ctx = canvas.getContext("2d");
-      const img = new Image();
+  try {
+    // Retrieve the canvas element from the provided reference
+    const canvas = canvasRef;
+    if (!canvas) return;
+    const ctx = canvas.getContext("2d");
 
-      img.onload = () => {
-        ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
-      };
-      img.src = `data:image/jpeg;base64,${frame}`;
-    } catch (error) {
-      console.error(error);
-    }
+    // Create a new image object and load the base64-encoded frame data
+    const img = new Image();
+
+    // Once the image is loaded, draw it onto the canvas
+    img.onload = () => {
+      ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
+    };
+
+    // Set the image source to the base64-encoded frame data
+    img.src = `data:image/jpeg;base64,${frame}`;
+  } catch (error) {
+    console.error(error);
+  }
 };
